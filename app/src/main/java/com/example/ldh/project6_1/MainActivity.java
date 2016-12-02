ef99c3d1ff5d2927dec616fb524f10fd0c96e6ac
201244076 LDH <7825dfg@naver.com>
2016-12-02 11:04:09 +0900

    레이아웃 생성

--- app/src/main/java/com/example/ldh/project6_1/MainActivity.java
+++ app/src/main/java/com/example/ldh/project6_1/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.ldh.project6_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
+    Chronometer Cmeter;
+    Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.button1);
+        btn2 = (Button)findViewById(R.id.button2);
+        Cmeter = (Chronometer)findViewById(R.id.chronometer);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
